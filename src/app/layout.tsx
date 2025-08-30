a5bcb0a5e90231816c6851a7515241ecd53a24fd
Dylan Brown <40218657+dylanbr0wn@users.noreply.github.com>
2025-08-30 00:14:04 -0700

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono, JetBrains_Mono } from 'next/font/google'
 import './globals.css'
 import Navbar from '../components/navbar'
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 export const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -43,6 +45,8 @@ export default function RootLayout({
         	{children}
         </div>
 			</body>
+      <SpeedInsights />
+      <Analytics />
 		</html>
 	)
 }
--- src/app/user-form.tsx
+++ src/app/user-form.tsx
@@ -1,33 +1,47 @@
 'use client'
 import Input from '@/components/input'
 import { useRouter, useSearchParams } from 'next/navigation'
-import { useState } from 'react'
+import { Suspense, useState } from 'react'
 
 export default function UserForm() {
-  const router = useRouter()
-  const searchParams = useSearchParams()
+	const router = useRouter()
 
-  function onSubmit(e: React.FormEvent) {
-    e.preventDefault()
-    const form = e.target as HTMLFormElement
-    const formData = new FormData(form)
-    const username = formData.get('user') as string
-    if (!username) return
-    router.push(`/${username}`)
-  }
+	function onSubmit(e: React.FormEvent) {
+		e.preventDefault()
+		const form = e.target as HTMLFormElement
+		const formData = new FormData(form)
+		const username = formData.get('user') as string
+		if (!username) return
+		router.push(`/${username}`)
+	}
 
 	return (
-    <form onSubmit={onSubmit} className='text-white flex flex-col gap-4 items-center z-10'>
-        <Input
-          name="user"
-          label="Last.fm Username"
-          required
-          placeholder="Somebody123"
-          error={searchParams.get('error') || undefined}
-        />
-        <button className='block border border-gray-200 p-2 w-32 text-base text-neutral-300 text-center bg-neutral-950 hover:bg-neutral-900' type='submit'>
-          Generate
-        </button>
-      </form> 
+		<form
+			onSubmit={onSubmit}
+			className="text-white flex flex-col gap-4 items-center z-10"
+		>
+			<Suspense>
+				<FormInput />
+			</Suspense>
+			<button
+				className="block border border-gray-200 p-2 w-32 text-base text-neutral-300 text-center bg-neutral-950 hover:bg-neutral-900"
+				type="submit"
+			>
+				Generate
+			</button>
+		</form>
+	)
+}
+
+function FormInput() {
+	const searchParams = useSearchParams()
+	return (
+		<Input
+			name="user"
+			label="Last.fm Username"
+			required
+			placeholder="Somebody123"
+			error={searchParams.get('error') || undefined}
+		/>
 	)
 }
