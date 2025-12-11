74c7e2e45758e6edc9f78f2071f609808412d3f4
Dylan Brown <40218657+dylanbr0wn@users.noreply.github.com>
2025-12-10 20:17:20 -0800

    fix

--- src/app/spotify/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { SessionProvider, useSession } from "next-auth/react";
-
-export default function Layout({ children }: { children: React.ReactNode, session: any }) {
-  return <div>
-    {children}
-  </div>
-}
